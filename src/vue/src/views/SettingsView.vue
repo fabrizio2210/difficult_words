c3e1a105d4f40ed369ee5e10f54d0d845899d861
Fabrizio W <fabrizio2210@gamil.com>
2024-04-28 19:28:00 +0100

    Resized settings

--- src/vue/src/views/SettingsView.vue
+++ src/vue/src/views/SettingsView.vue
@@ -4,11 +4,11 @@ import { useOpensubtitlesStore } from "../stores/opensubtitles";
 
 <template>
   <div class="setting">
-    <h1>This is an setting page</h1>
+    <h1>Settings</h1>
     <ul>
       <li v-for="setting in settings" :key="setting.key">
-        <label :for="setting.key">{{ setting.key }} :</label>
-        <input :id="setting.key" v-model="setting.value" />
+        <label class="desc" :for="setting.key">{{ setting.key }} :</label>
+        <input :id="setting.key" v-model="setting.value" size="30"/>
       </li>
     </ul>
     <div>
@@ -44,6 +44,9 @@ export default {
 </script>
 
 <style>
+.desc {
+  padding: 10px;
+}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
