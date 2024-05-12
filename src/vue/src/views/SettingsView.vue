d49be5b1f7eb9297261f87a262f7c55438057a5d
Fabrizio W <fabrizio2210@gamil.com>
2024-05-12 16:11:22 +0100

    Added tooltip

--- src/vue/src/views/SettingsView.vue
+++ src/vue/src/views/SettingsView.vue
@@ -7,7 +7,10 @@ import { useOpensubtitlesStore } from "../stores/opensubtitles";
     <h1>Settings</h1>
     <ul>
       <li v-for="setting in settings" :key="setting.key">
-        <label class="desc" :for="setting.key">{{ setting.key }} :</label>
+        <div class="tooltip">
+          <label class="desc" :for="setting.key">{{ setting.key }} :</label>
+          <span class="tooltiptext" v-html="setting.description"></span>
+        </div>
         <input :id="setting.key" v-model="setting.value" size="30" />
       </li>
     </ul>
@@ -21,7 +24,11 @@ import { useOpensubtitlesStore } from "../stores/opensubtitles";
 export default {
   data() {
     return {
-      settings: [{ key: "API_KEY", value: "" }],
+      settings: [
+      { key: "API_KEY",
+        value: "",
+        description: "API key to query OpenSubtitles. Follow instruction at <a href='https://opensubtitles.stoplight.io/docs/opensubtitles-api/e3750fd63a100-getting-started' target='_blank'>their documentation</a>", 
+      }],
     };
   },
   methods: {
@@ -51,6 +58,35 @@ ul {
 .desc {
   padding: 10px;
 }
+/* Tooltip container */
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+/* Tooltip text */
+.tooltip .tooltiptext {
+  width: 240px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  padding: 5px;
+  border-radius: 6px;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 1s;
+
+  /* Position the tooltip text - see examples below! */
+  position: absolute;
+  z-index: 1;
+}
+
+/* Show the tooltip text when you mouse over the tooltip container */
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 0.75;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
